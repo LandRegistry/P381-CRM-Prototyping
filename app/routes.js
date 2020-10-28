c3bfd303d435294b5f2e071a805d0656e50f0a78
Andy Carr <hello@andycarr.design>
2020-10-28 18:05:19 +0000

    Setup-routing

--- app/routes.js
+++ app/routes.js
@@ -3,4 +3,26 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+router.post('/UserResearch/ContactformQA/public-or-business-choice', function(req, res) {
+
+    const introductionChoice = req.session.data['public-business']
+// Add
+switch(introductionChoice)
+{
+    case 'public-customer':
+        res.redirect('/UserResearch/ContactformQA/Public/');
+        break;
+
+    case 'business-customer':
+        res.redirect('/UserResearch/ContactformQA/Business/');
+        break; 
+        
+    default:
+    //This is a catch all for none of the above
+        break;
+}
+ 
+
+})
+
 module.exports = router
