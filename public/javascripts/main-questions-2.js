4a2ee355a51b2c95b654f0fd098bf2a6dc5df51a
Andy Carr <hello@andycarr.design>
2021-01-05 18:20:12 +0000

    updates

---
+++ public/javascripts/main-questions-2.js
@@ -0,0 +1,12 @@
+const topics = () => [
+    {
+          "subject": "Applications",
+          "question": "What is the register?",
+          "answer": "The Land Register contains more than 25 million titles showing evidence of ownership for more than 87% of the land mass of England and Wales."
+      },
+      {
+          "subject": "Applications",
+          "question": "How do I update or correct the register?",
+          "answer": " If anything in the register changes or it is incorrect you can <a href='https://www.google.com/url?q=https%3A%2F%2Fwww.gov.uk%2Fregistering-land-or-property-with-land-registry%2Fupdate-or-correct-the-register' target='_blank'>update or correct the register</a>."
+      }
+  ]
